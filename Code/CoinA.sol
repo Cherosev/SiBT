e1baaf05b0e65034c0d8029239f43102f5078381
Remix Workspace <>
2022-08-15 13:36:31 +0200

    Tilføjet ny kontrakt der kun holder styr på hvad der må sælges hvor.

--- Code/CoinA.sol
+++ Code/CoinA.sol
@@ -1,90 +1,127 @@
+// SPDX-License-Identifier: DIKU
+pragma solidity ^0.8.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/utils/Context.sol";
 
-contract ERC20 {
 
-    enum CoinTypes{A, B, C}
+contract WaterCoin is IERC20, Context {
 
-    uint256 _CoinASupply;
-    uint256 _CoinBSupply;
-    uint256 _CoinCSupply;
+    mapping (address => uint256) private _balances;
 
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 _currentSupply;
     uint256 _totalSupply;
 
-    address _owner;
+    string _name;
+    string _symbol;
 
-    mapping (address => mapping (CoinTypes => uint256)) _balances;
-
-
-    event Transfer(CoinTypes coin, address indexed from, address indexed to, uint256 value);
-
-    constructor() {
-        _owner = msg.sender;
+    constructor (string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
     }
 
-    modifier onlyOwner() {
-        require(_owner == msg.sender, "sender is not owner");
-        _;
+    function name() public view virtual returns (string memory) {
+        return _name;
     }
 
-    modifier invariance() {
-        uint256 oldTotalSupply = _totalSupply;
-        _;
-        uint256 newTotalSupply = _totalSupply;
-        require (oldTotalSupply == newTotalSupply, "invariance found");
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
     }
 
-    function name() external pure returns (string memory) {
-        return "Water Coin";
-    }
-
-    function symbol() external pure returns (string memory) {
-
-        return "WAC";
-    }
-
-    function decimals () external pure returns (uint) {
+    function decimals() public view virtual returns (uint8) {
         return 18;
     }
 
-    function totalSupply() external view returns (uint256) {
-        return _totalSupply;
+    function totalSupply() public view virtual override returns (uint256) {
+        return _currentSupply;
     }
 
-    function balanceOf(CoinTypes coin, address _person) external view returns (uint256) {
-        return _balances[_person][coin];
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
     }
 
-    function transfer (CoinTypes coin, address _to, uint256 _value) external invariance returns (bool) {
-        address from = msg.sender;
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
 
-        require(_balances[from][coin] >= _value);
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
 
-        _balances[from][coin] -= _value;
-        _balances[_to][coin] += _value;
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
 
-        emit Transfer(coin, from, _to, _value);
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "WaterCoin: transfer amount exceeds allowance");
+        _approve(sender, _msgSender(), currentAllowance - amount);
 
         return true;
     }
 
-    function mint(CoinTypes coin, uint256 _value) external onlyOwner returns (bool) {
-        address from = msg.sender;
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
 
-        require(_owner == from, "sender is not owner");
-
-        _balances[from][coin] += _value;
-        _totalSupply += _value;
-
-        if (coin == CoinTypes.A){
-            _CoinASupply += _value;
-        }
-        if (coin == CoinTypes.B){
-            _CoinBSupply += _value;
-        }
-        if (coin == CoinTypes.C) {
-            _CoinCSupply += _value;
-        }
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        uint256 currentAllowance = _allowances[_msgSender()][spender];
+        require(currentAllowance >= subtractedValue, "WaterCoin: decreased allowance below zero");
+        _approve(_msgSender(), spender, currentAllowance - subtractedValue);
 
         return true;
     }
 
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+        require(sender != address(0), "WaterCoin: transfer from the zero address");
+        require(recipient != address(0), "WaterCoin: transfer to the zero address");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "WaterCoin: transfer amount exceeds balance");
+        _balances[sender] = senderBalance - amount;
+        _balances[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "WaterCoin: burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "WaterCoin: burn amount exceeds balance");
+        _balances[account] = accountBalance - amount;
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "WaterCoin: approve from the zero address");
+        require(spender != address(0), "WaterCoin: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
 }
